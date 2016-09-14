ffb62824c4945f9a242e6f72d82bcbb7100848bb
Ailuridaes <ailuridaes@gmail.com>
2016-09-14 09:47:22 -0700

    added ui-router, template files

--- src/app/app.js
+++ src/app/app.js
@@ -2,5 +2,18 @@
     'use strict';
 
     angular
-        .module('app', []);
+        .module('app', ['ui.router'])
+
+        .config(function($stateProvider, $urlRouterProvider){
+        	$urlRouterProvider.otherwise('/feature');
+
+        	$stateProvider
+        	.state('feature', {
+        		url: '/feature',
+        		templateUrl: '/app/feature/feature.html',
+        		controller: 'FeatureController as feature'
+        	});
+
+        });
+
 })();
